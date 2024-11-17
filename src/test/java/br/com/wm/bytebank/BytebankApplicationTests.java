6e01d9582658ac0c4fcd5911f7c1c84cf4b03ea9
williammian <william_mian@yahoo.com.br>
2024-11-17 15:10:55 -0300

    spring batch

---
+++ src/test/java/br/com/wm/bytebank/BytebankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wm.bytebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
